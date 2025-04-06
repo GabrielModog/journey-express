56249acdcbe84eab91982039aa20d8f225471dd6
GabrielModog <modog.dev@gmail.com>
2025-04-06 06:43:41 -0300

    feat: add frontend base

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import '@mantine/core/styles.css';
+
+import React from 'react';
+import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
+import { theme } from '@/utils/theme';
+
+export const metadata = {
+  title: 'Jornadas',
+  description: 'Aplicação para gerenciar jornadas',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
